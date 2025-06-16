8295a4606d410756ad081c713ba8fe1b28ce3592
Tofazzal Hossain <mdtofazzalislam6789@gmail.com>
2025-06-16 12:22:09 +0600

    user login oparation all validation finish

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,52 @@
 import express from "express";
+import connectDB from "../db/connectDB.js";
+import UserModel from "./../models/UserModel/UserModel.js";
+import bcrypt from "bcrypt";
+
 const authRouter = express.Router();
 
-authRouter.get("/", (req, res) => {
-  res.send({ message: "Auth route is working" });
+authRouter.post("/validation", async (req, res) => {
+  try {
+    await connectDB();
+    const user = req.body;
+    const currentUser = await UserModel.findOne({ email: user?.email });
+    if (currentUser) {
+      const isPasswordMatched = bcrypt.compareSync(
+        user.password,
+        currentUser.password
+      );
+      if (isPasswordMatched) {
+        return res.send({
+          message: "User Register Successfully",
+          success: false,
+          status: 200,
+        });
+      } else {
+        return res.send({
+          message: "User Already Exists but Password is Incorrect",
+          success: false,
+          status: 403,
+        });
+      }
+    }
+    const hashPassword = bcrypt.hashSync(user.password, 14);
+    const newUser = new UserModel({ ...user, password: hashPassword });
+    await newUser.save();
+
+    res.send({
+      data: newUser,
+      message: "User Created Successfully",
+      success: true,
+      status: 201,
+    });
+  } catch (error) {
+    console.error("Registration Error", error.message);
+    res.send({
+      message: "There was a server error",
+      success: false,
+      status: 500,
+    });
+  }
 });
 
 export default authRouter;
